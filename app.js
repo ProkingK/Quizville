a3556027e4b2f6dede9eab0ed9239087974302d3
Ashley Kapaso <ashleykapaso@gmail.com>
2023-06-26 12:07:32 +0200

    add styling to splash page

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import routes from './routes/routes.js';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use('/', routes);
-app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
