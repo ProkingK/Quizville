3744ce1c804b77eddf1349eb93d094b6eeeb5731
Ashley Kapaso <ashleykapaso@gmail.com>
2023-07-09 20:40:53 +0200

    add security to requests

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,31 +1,52 @@
 import Post from '../models/postModel.js';
 
 export const add = (req, res) => {
+
+    const token = req.cookies.token;
+
     try {
-        const { username, profilePhoto, content, media} = req.body;
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
 
-        const newPost = new Post({
-            username,
-            profilePhoto,
-            content,
-            media
-        });
-      
-        Post.create(newPost);
+        try {
+            const { username, profilePhoto, content, media } = req.body;
 
-        res.status(201).json({ success: true, post: newPost });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+            const newPost = new Post({
+                username,
+                profilePhoto,
+                content,
+                media
+            });
+
+            Post.create(newPost);
+
+            res.status(201).json({ success: true, post: newPost });
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(401).json({ message: 'Invalid token' });
     }
 };
 
 export const getAll = async (req, res) => {
-    try {
-        const posts = await Post.find({});
+    const token = req.cookies.token;
 
-        res.status(201).json({ success: true, posts: posts });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+        try {
+            const posts = await Post.find({});
+
+            res.status(201).json({ success: true, posts: posts });
+        }
+        catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
     }
     catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        console.log(error);
+        res.status(401).json({ message: 'Invalid token' });
     }
 };
